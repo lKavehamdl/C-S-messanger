42b54dc1d46bf21cc85471221bada758a46e5c1b
= <=>
2025-05-12 22:37:51 +0330

    first commit

---
+++ client.py
@@ -0,0 +1,143 @@
+import socket
+import threading
+import json
+import time
+
+chatting = False
+
+def send_message(sock, data):
+    sock.send(json.dumps(data).encode())
+
+def recv_message(sock):
+    try:
+        return json.loads(sock.recv(4096).decode())
+    except:
+        return None
+
+def chat_session(sock, peer_name):
+    global chatting
+    chatting = True
+    time.sleep(0.3)
+    print(f"[Chat with {peer_name} started. Type '#' to exit.]")
+
+    def listen():
+        global chatting
+        while chatting:
+            msg = recv_message(sock)
+            if not msg:
+                break
+            if msg["type"] == "CHAT_MESSAGE":
+                time.sleep(0.3)
+                print(f"[{msg['from']}] {msg['text']}")
+            elif msg["type"] == "CHAT_ENDED":
+                time.sleep(0.3)
+                print("\n[Chat ended. Returning to menu.]")
+                chatting = False
+                break
+
+    t = threading.Thread(target=listen, daemon=True)
+    t.start()
+
+    while chatting:
+        try:
+            time.sleep(0.3)
+            text = input()
+            send_message(sock, {"type": "CHAT_MESSAGE", "text": text})
+            if text == "#":
+                chatting = False
+                break
+        except:
+            break
+
+def background_listener(sock):
+    global chatting
+    while True:
+        msg = recv_message(sock)
+        if not msg:
+            break
+
+        if msg["type"] == "CHAT_INVITE":
+            from_user = msg["from"]
+            time.sleep(0.3)
+            print(f"\n[Incoming chat request from {from_user}]")
+            time.sleep(.3)
+            choice = input("Accept? (y/n): ").strip().lower()
+            if choice == "y":
+                send_message(sock, {"type": "CHAT_ACCEPTED"})
+                time.sleep(.3)
+                chat_session(sock, from_user)  # <- Now actually enter session
+            else:
+                send_message(sock, {"type": "CHAT_DECLINED"})
+
+def main():
+    global chatting
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 1111))
+
+    # Username login
+    while True:
+        msg = recv_message(client)
+        print(msg)
+        if msg["type"] == "REQUEST_USERNAME":
+            username = input("Enter a unique username: ")
+            send_message(client, {"type": "USERNAME", "username": username})
+        elif msg["type"] == "INVALID_USERNAME":
+            print("Username invalid or taken.")
+        elif msg["type"] == "USERNAME_ACCEPTED":
+            print("Connected to server.")
+            break
+
+    threading.Thread(target=background_listener, args=(client,), daemon=True).start()
+
+    while True:
+        if chatting:
+            continue
+        print("\n--- Menu ---")
+        print("1. Show users")
+        print("2. Chat with someone")
+        print("3. Change username")
+        print("4. Exit")
+        choice = input("Choice [1-4]: ")
+
+        if choice == "1":
+            send_message(client, {"type": "SHOW_USERS"})
+            res = recv_message(client)
+            if res and res.get("type") == "USER_LIST":
+                users = res["users"]
+                print("Online users:", ", ".join(users) if users else "None")
+
+        elif choice == "2":
+            send_message(client, {"type": "SHOW_USERS"})
+            res = recv_message(client)
+            if res and res.get("type") == "USER_LIST":
+                users = res["users"]
+                if not users:
+                    print("No available users.")
+                    continue
+                print("Online users:", ", ".join(users))
+                target = input("Enter username to chat with: ")
+                send_message(client, {"type": "CHAT_REQUEST", "target": target})
+                result = recv_message(client)
+                if result and result.get("type") == "CHAT_STARTED":
+                    chat_session(client, target)
+                elif result and result.get("type") == "ERROR":
+                    print("Error:", result["message"])
+
+        elif choice == "3":
+            newname = input("New username: ")
+            send_message(client, {"type": "RENAME", "new_username": newname})
+            res = recv_message(client)
+            if res and res.get("type") == "USERNAME_CHANGED":
+                print("Username updated:", res["new_username"])
+            elif res:
+                print("Error:", res.get("message", "Unknown error"))
+
+        elif choice == "4":
+            send_message(client, {"type": "EXIT"})
+            break
+
+    client.close()
+    print("Disconnected.")
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,144 @@
+import socket
+import threading
+import json
+
+clients = {}  # username: {'socket': ..., 'addr': ..., 'port': ..., 'thread': ..., 'in_chat': False}
+lock = threading.Lock()
+
+def log(message):
+    with open("log.txt", "a") as f:
+        f.write(message + "\n")
+
+
+def send_message(sock, data):
+    try:
+        sock.send(json.dumps(data).encode())
+    except:
+        pass
+
+def recv_message(sock):
+    try:
+        data = sock.recv(4096)
+        return json.loads(data.decode()) if data else None
+    except:
+        return None
+
+def list_online_users(exclude=None):
+    with lock:
+        return [u for u in clients if u != exclude and not clients[u]['in_chat']]
+
+def chat_session(user1, user2):
+    sock1 = clients[user1]['socket']
+    sock2 = clients[user2]['socket']
+    clients[user1]['in_chat'] = True
+    clients[user2]['in_chat'] = True
+
+    send_message(sock1, {"type": "CHAT_STARTED", "with": user2})
+    send_message(sock2, {"type": "CHAT_STARTED", "with": user1})
+
+    def forward(source_sock, dest_sock, source_user):
+        while True:
+            msg = recv_message(source_sock)
+            if not msg or msg.get("text") == "#":
+                send_message(dest_sock, {"type": "CHAT_ENDED"})
+                send_message(source_sock, {"type": "CHAT_ENDED"})
+                break
+            send_message(dest_sock, {"type": "CHAT_MESSAGE", "from": source_user, "text": msg["text"]})
+
+    t1 = threading.Thread(target=forward, args=(sock1, sock2, user1))
+    t2 = threading.Thread(target=forward, args=(sock2, sock1, user2))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    clients[user1]['in_chat'] = False
+    clients[user2]['in_chat'] = False
+
+def handle_client(client_sock, addr):
+    username = None
+
+    # Request unique username
+    while True:
+        send_message(client_sock, {"type": "REQUEST_USERNAME"})
+        response = recv_message(client_sock)
+        if not response or response.get("type") != "USERNAME":
+            continue
+        name = response["username"]
+        with lock:
+            if name.isalnum() and name not in clients:
+                username = name
+                clients[username] = {
+                    "socket": client_sock,
+                    "addr": addr[0],
+                    "port": addr[1],
+                    "in_chat": False
+                }
+                send_message(client_sock, {"type": "USERNAME_ACCEPTED"})
+                log(f"[+] {username} connected from {addr}")
+                break
+            else:
+                send_message(client_sock, {"type": "INVALID_USERNAME"})
+
+    try:
+        while True:
+            msg = recv_message(client_sock)
+            if not msg:
+                break
+
+            if msg["type"] == "SHOW_USERS":
+                online = list_online_users(username)
+                send_message(client_sock, {"type": "USER_LIST", "users": online})
+
+
+            elif msg["type"] == "CHAT_REQUEST":
+                target = msg["target"]
+                with lock:
+                    if target not in clients or clients[target]["in_chat"]:
+                        send_message(client_sock, {"type": "ERROR", "message": "User not available."})
+                        continue
+                    target_sock = clients[target]["socket"]
+                    send_message(target_sock, {"type": "CHAT_INVITE", "from": username})
+
+                    reply = recv_message(target_sock)
+                    if reply and reply.get("type") == "CHAT_ACCEPTED":
+                        chat_session(username, target)
+                    else:
+                        send_message(client_sock, {"type": "ERROR", "message": f"{target} declined the chat."})
+
+            elif msg["type"] == "RENAME":
+                new_name = msg["new_username"]
+                with lock:
+                    if new_name.isalnum() and new_name not in clients:
+                        clients[new_name] = clients.pop(username)
+                        username = new_name
+                        send_message(client_sock, {"type": "USERNAME_CHANGED", "new_username": new_name})
+                    else:
+                        send_message(client_sock, {"type": "ERROR", "message": "Invalid or taken username."})
+
+            elif msg["type"] == "EXIT":
+                break
+
+    except:
+        pass
+    finally:
+        with lock:
+            if username in clients:
+                del clients[username]
+        client_sock.close()
+        log(f"[-] {username} disconnected")
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", 1111))
+    server.listen()
+    print("[*] Server listening on port 5555")
+    log("[*] Server listening on port 5555")
+    
+
+    while True:
+        client_sock, addr = server.accept()
+        threading.Thread(target=handle_client, args=(client_sock, addr)).start()
+
+if __name__ == "__main__":
+    main()
